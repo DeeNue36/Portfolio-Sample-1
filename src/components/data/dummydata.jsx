e2996ee7745db166fa3551fbc7941299a433f1ef
Dugiii <dugidemeg@gmail.com>
2025-12-24 01:57:53 -0600

    added more dummy data

--- src/components/data/dummydata.jsx
+++ src/components/data/dummydata.jsx
@@ -42,3 +42,50 @@ export const home = [
         longDesc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores porro nostrum enim tenetur sequi aliquam dolor nemo, nisi harum esse ducimus similique, ipsam quo! Sequi odit libero rerum quis itaque.",
     },
 ]
+
+export const about = [
+    {
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam et non doloribus voluptate eveniet. Ipsa exercitationem provident quos quibusdam ad id aperiam voluptatibus illo nesciunt consectetur error, assumenda cupiditate. Voluptatum?",
+        desc1: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et enim officiis maiores quae nesciunt? Animi, voluptatem. Esse minima optio dolor autem, quod, eos quas cumque molestias similique, pariatur error! Temporibus!",
+        cover: "../../assets/images/creative_1.jpg"
+    },
+]
+
+export const services = [
+    {
+        id: 1,
+        icon: <Settings />,
+        title: "Web Development",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, repellat.",
+    },
+    {
+        id: 2,
+        icon: <CropRotate />,
+        title: "Clean Code",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, repellat.",
+    },
+    {
+        id: 3,
+        icon: <ViewInAr />,
+        title: "Responsive Design",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, repellat.",
+    },
+    {
+        id: 4,
+        icon: <PieChart />,
+        title: "Performance Optimization",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, repellat.",
+    },
+    {
+        id: 5,
+        icon: <Code />,
+        title: "CSS Processors(Tailwind, Bootstrap etc.)",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, repellat.",
+    },
+    {
+        id: 6,
+        icon: <BarChart />,
+        title: "Amazing Support",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, repellat.",
+    },
+]
