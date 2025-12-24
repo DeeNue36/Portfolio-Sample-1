db99bdcced11707dbc72e67d0ab411e342f70c2e
Dugiii <dugidemeg@gmail.com>
2025-12-23 21:18:36 -0600

    website content data

---
+++ src/components/data/dummydata.jsx
@@ -0,0 +1,44 @@
+import { Settings, CropRotate, ViewInAr, PieChart, Code, BarChart, CloudOutlined, FavoriteBorder, Public, PersonOutlined, AddLocationAltOutlined,
+PhoneIphone, EmailOutlined, Facebook, Twitter, Instagram, YouTube } from "@amui/icons-material"
+
+
+export const navlinks = [
+    {
+        url: "/",
+        text: "Home",
+    },
+    {
+        url: "/about",
+        text: "About",
+    },
+    {
+        url: "/services",
+        text: "Services",
+    },
+    {
+        url: "/portfolio",
+        text: "Portfolio",
+    },
+    {
+        url: 'testimonials',
+        text: 'Testimonials',
+    },
+    {
+        url: '/blog',
+        text: 'Blog',
+    },
+    {
+        url: '/contact',
+        text: 'Contact',
+    }
+]
+
+export const home = [
+    {
+        text: 'Hi there, I am',
+        name: 'Dugidem Egwuatu',
+        post: 'Frontend Developer',
+        shortDesc: 'I build things for the web',
+        longDesc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores porro nostrum enim tenetur sequi aliquam dolor nemo, nisi harum esse ducimus similique, ipsam quo! Sequi odit libero rerum quis itaque.",
+    },
+]
