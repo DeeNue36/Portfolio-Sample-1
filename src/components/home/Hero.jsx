0fee72d8e6f6fcebf32ea884a42390d83b976d98
Dugiii <dugidemeg@gmail.com>
2026-01-03 16:38:18 -0600

    imports and hero section layout

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import { home } from '../data/dummydata'
+import Typewriter from 'typewriter-effect'
 
 export const Hero = () => {
     return (
-        <div>Hero</div>
+        <>
+            <section className="hero">
+                {home.map((homeVal, index) => (
+                    <div className="heroContainer">
+                        <h2>{homeVal.text} </h2>
+                    <h1>
+                        <Typewriter 
+                            options = {{ strings: [`${homeVal.name}`, `${homeVal.post}`, `${homeVal.shortDesc}`], autoStart: true, loop: true }} 
+                        />
+                    </h1>
+                    <p>{homeVal.longDesc}</p>
+                    <button className="primaryBtn">
+                        Projects
+                    </button>
+                    </div>
+                ))}
+            </section>
+        </>
     )
 }
